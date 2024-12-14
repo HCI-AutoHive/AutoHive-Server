3f620011aa241cbdf504c1d43907f620a19a5681
JoonYong <joonyongpark99@gmail.com>
2024-12-15 05:46:16 +0900

    [#9] FEAT: 크롤링 기반 Xlsx 데이터 DB 초기화 구현

---
+++ src/main/java/com/hci/autohive/controller/ExcelController.java
@@ -0,0 +1,34 @@
+package com.hci.autohive.controller;
+
+import com.hci.autohive.service.ExcelService;
+import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/excel")
+public class ExcelController {
+
+  private final ExcelService excelDataService;
+
+  public ExcelController(ExcelService excelDataService) {
+    this.excelDataService = excelDataService;
+  }
+
+  @PostMapping("/upload")
+  public ResponseEntity<String> uploadExcel(@RequestParam("file") MultipartFile file) {
+    try {
+      excelDataService.saveExcelData(file);
+      return ResponseEntity.ok("successfully.");
+    } catch (IOException e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("Failed");
+    }
+  }
+}
+
---
+++ src/main/java/com/hci/autohive/service/ExcelService.java
@@ -0,0 +1,52 @@
+package com.hci.autohive.service;
+
+import com.hci.autohive.domain.Car;
+import com.hci.autohive.domain.Detail;
+import com.hci.autohive.repository.CarRepository;
+import com.hci.autohive.repository.DetailRepository;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ExcelService {
+
+  private final CarRepository carRepository;
+  private final DetailRepository detailRepository;
+
+  public ExcelService(CarRepository carRepository, DetailRepository detailRepository) {
+    this.carRepository = carRepository;
+    this.detailRepository = detailRepository;
+  }
+
+  @Transactional
+  public void saveExcelData(MultipartFile file) throws IOException {
+
+    Workbook workbook = new XSSFWorkbook(file.getInputStream());
+    Sheet sheet = workbook.getSheetAt(0);
+
+    for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+      Row row = sheet.getRow(i);
+
+      Car car = new Car();
+      car.setModel(row.getCell(0).getStringCellValue());
+      car.setStarRating((float) row.getCell(1).getNumericCellValue());
+      car.setImageUrl(row.getCell(4).getStringCellValue());
+      Car savedCar = carRepository.save(car);
+
+      Detail detail = new Detail();
+      detail.setCar(savedCar);
+      detail.setSafety(row.getCell(3).getStringCellValue());
+      detail.setPerform(row.getCell(2).getStringCellValue());
+      detailRepository.save(detail);
+
+    }
+
+    workbook.close();
+  }
+}
